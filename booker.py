36654b470c45f7cd9002fffbce1fbf336f8eaebb
vsuri <vijaysaikrishna.suri@factset.com>
2021-12-15 11:09:34 +0530

    add logs

--- booker.py
+++ booker.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from playsound import playsound
 import time
 
-pushpa_url = 'https://in.bookmyshow.com/movies/pushpa-the-rise-part-01/ET00129538'
+pushpa_url = 'https://in.bookmyshow.com/guntur/movies/pushpa-the-rise-part-01/ET00129538'
 far_from_url = 'https://in.bookmyshow.com/guntur/movies/spider-man-no-way-home/ET00310790'
 request  = requests.get(pushpa_url)
 htmlContent = request.text
@@ -16,6 +16,8 @@ book_tickets_btn = soup.find('button', {'data-phase':"postRelease"})
 print(book_tickets_btn)
 
 while True:
+    print('checking')
+    print(book_tickets_btn)
     if book_tickets_btn:
         playsound('BabyElephantWalk60.wav')
         print('playing sound using  playsound')
